5d4f9909194446e71b26b018e32378f8c62c8d2d
KruthanReddy <kruthanreddy812@gmail.com>
2025-04-12 19:02:56 -0500

    Delete submit.php

--- submit.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    echo "<p>Thank you, $name. We will contact you at $email.</p>";
-}
-?>
